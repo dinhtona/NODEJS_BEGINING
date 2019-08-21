25dc6850cd7e3a118c964e59d2eb51d922e2389c
dinhtona <dinhtona@gmail.com>
2019-08-21 15:56:23 +0700

    Get params and query string in URL in request from client

--- vid68_express/app.js
+++ vid68_express/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
+var fs = require('fs');
 
 var app = express();
 app.set('view engine', 'ejs'); //express'll auto find views folder, it is has template file
@@ -21,7 +22,14 @@ app.use('/assets', express.static(__dirname + '/public'));//midleware: tạo ra 
 
 //functions
 const index = (req, res) => {
-    res.render('index');//access to views folder and get index.ejs file
+    //res.render('index');//access to views folder and get index.ejs file
+
+    //get stream file
+    var rIndex = fs.createReadStream(__dirname + '/index.html', {
+        encoding: 'utf-8',
+        highWaterMark: 16 * 1024
+    });
+    rIndex.pipe(res);
 }
 //end functions
 
@@ -42,11 +50,16 @@ app.get('/user', (req, res) => {
     res.json(user);//giống như trên
 }); //test save
 
+/***
+ * ex: http://localhost:2000/user/5?qstr=this_is_query_string&qstr2=9999&qstr=88888
+ * req.params: /user/:id <=> /5
+ * req.query: ?query_name <=> ?qstr
+ */
 app.get('/user/:id', (req, res) => {// /:name
     res.cookie("username", req.params.id);
     //res.send(req.params); //lấy tham số client truyền vào: req.params.id
     // res.send(`User ID: ${req.params.id}`);
-    res.render('user', { ID: req.params.id });
+    res.render('user', { ID: req.params.id, queryString: req.query.qstr });
 });
 
 app.listen(port, () => {
