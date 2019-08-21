cf85c73edbb4c97fdfe9a438ea7ef175c4eb3d4c
dinhtona <dinhtona@gmail.com>
2019-08-21 10:07:34 +0700

    Midleware express: static file and custom midlewawre

--- vid68_express/app.js
+++ vid68_express/app.js
@@ -4,9 +4,24 @@ var app = express();
 
 var port = 2000;
 
+app.use('/assets', express.static(__dirname + '/public'));//midleware: tạo ra một cầu nối mà mỗi khi client nhập vào từ url có  /assets thì sẽ truy cập vào tên file sau đó
+
+//custom midleware: write log when client request
+app.use('/', (req, res, next) => { //next : bắt buộc phải có để có thể chạy tiếp các midleware khác nữa
+    var ip = req.connection.remoteAddress;
+    console.log('IP: ' + ip + '\nRequest URL: ' + req.url);
+    req.requestTime = new Date().getTime();
+
+    next();
+});
+
 //functions
 const index = (req, res) => {
-    res.send('<h1>Express Server Node JS HOME</h1>');
+    res.send(`
+        <link rel='stylesheet' type="text/css" href='/assets/style.css' />
+        <h1>Express Server Node JS HOME</h1>
+        <p>Request time: ${req.requestTime}</p>
+    `);//href='/assets/style.css' : Truy cập tới file style.css trong public đã dược map thành /assest/
 }
 //end functions
 
