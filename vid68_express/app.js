6540dab006f4509977fab39436143af546b1737b
dinhtona <dinhtona@gmail.com>
2019-08-21 10:27:52 +0700

    install a midleware into app : cookie-parser

--- vid68_express/app.js
+++ vid68_express/app.js
@@ -1,19 +1,22 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
 var port = 2000;
 
-app.use('/assets', express.static(__dirname + '/public'));//midleware: tạo ra một cầu nối mà mỗi khi client nhập vào từ url có  /assets thì sẽ truy cập vào tên file sau đó
-
 //custom midleware: write log when client request
 app.use('/', (req, res, next) => { //next : bắt buộc phải có để có thể chạy tiếp các midleware khác nữa
-    var ip = req.connection.remoteAddress;
+    var ip = req.headers['x-real-ip'] || req.connection.remoteAddress;;
     console.log('IP: ' + ip + '\nRequest URL: ' + req.url);
     req.requestTime = new Date().getTime();
 
     next();
 });
+
+app.use(cookieParser());
+
+app.use('/assets', express.static(__dirname + '/public'));//midleware: tạo ra một cầu nối mà mỗi khi client nhập vào từ url có  /assets thì sẽ truy cập vào tên file sau đó
 
 //functions
 const index = (req, res) => {
@@ -21,6 +24,8 @@ const index = (req, res) => {
         <link rel='stylesheet' type="text/css" href='/assets/style.css' />
         <h1>Express Server Node JS HOME</h1>
         <p>Request time: ${req.requestTime}</p>
+
+        <p>Cookies: ${JSON.stringify(req.cookies)}</p>
     `);//href='/assets/style.css' : Truy cập tới file style.css trong public đã dược map thành /assest/
 }
 //end functions
@@ -43,6 +48,7 @@ app.get('/user', (req, res) => {
 }); //test save
 
 app.get('/user/:id', (req, res) => {// /:name
+    res.cookie("username", req.params.id);
     //res.send(req.params); //lấy tham số client truyền vào: req.params.id
     res.send(`User ID: ${req.params.id}`);
 });
